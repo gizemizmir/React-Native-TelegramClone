eb3d6d52810d6f84e2b3f8860622237348a529b6
Gizem Izmir <gizkes1996@gmail.com>
2022-08-29 15:50:20 +0300

    React Native project created

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+        <View>
+          <Text>App.js</Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
