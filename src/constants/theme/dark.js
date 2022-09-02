ba112b38123da405173b8d9a353a7f26574dd3a1
Gizem Izmir <gizkes1996@gmail.com>
2022-09-02 10:45:53 +0300

    theme files updated

--- src/constants/theme/dark.js
+++ src/constants/theme/dark.js
@@ -2,5 +2,6 @@ export default {
   type: 'dark',
   backgroundColor: '#14202b',
   color: '#FFF',
+  greyText: '#CCC',
   tintColor: 'rgb(29, 155, 240)',
 };
--- src/constants/theme/light.js
+++ src/constants/theme/light.js
@@ -2,5 +2,6 @@ export default {
   type: 'light',
   backgroundColor: '#FFF',
   color: '#000',
+  greyText: '#333',
   tintColor: 'rgb(29, 155, 240)',
 };
